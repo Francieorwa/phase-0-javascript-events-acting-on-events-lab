2171b376abc8702c9e69273df54d2781aa0462ca
Francisca <francisca.orwa@student.moringaschool.com>
2022-11-29 13:35:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft(){
+    dodger.style.backgroundColor = "#FF69B4";
+    dodger.style.bottom = "0px";
+    dodger.style.left = "0px";
+
+}
+function moveDodgerRight(){
+    const rightNumbers =dodger.style.left.replace("px","");
+    const right = parseInt(rightNumbers,10);
+
+    if(right>=0){
+        dodger.style.left =`${right +1}px`;
+    }
+
+}
+document.addEventListener("keyup",function(e){
+    if (e.key ==="ArrowRight"){
+        moveDodgerLeft()
+    }
+});
